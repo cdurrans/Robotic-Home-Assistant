f27a5a8e88e59a0b45b95c8c4c8db7d33bb75df2
root <durranschristopher@gmail.com>
2021-01-22 14:02:39 +0000

    corrected scripts

--- catkin_ws/src/scripts/test_navigation.sh
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find gmapping)/../../my_robot/worlds/myhome.world" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/turtlebot_simulator/turtlebot_gazebo/worlds/myhome.world" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/src/turtlebot_simulator/turtlebot_gazebo/maps/map.yaml" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
