7ef08c839ada1ec221f5121933722bba12683c80
Chris Durrans <durranschristopher@gmail.com>
2021-01-25 12:54:52 -0700

    Update world file locations

--- catkin_ws/src/scripts/home_service.sh
+++ catkin_ws/src/scripts/home_service.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/turtlebot_simulator/turtlebot_gazebo/worlds/myhome.world" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/myhome.world" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/src/turtlebot_simulator/turtlebot_gazebo/maps/map.yaml" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find turtlebot_gazebo)/maps/map.yaml" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/turtlebot_simulator/turtlebot_gazebo/worlds/myhome.world" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/myhome.world" &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
